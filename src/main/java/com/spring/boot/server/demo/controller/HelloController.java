5dca482aa3b02f44f75a4045a9b8da5a2d6a5419
GuoXin <xin.guo@after615.com>
2017-09-19 18:45:54 +0800

    multi-env&properties

--- src/main/java/com/spring/boot/server/demo/controller/HelloController.java
+++ src/main/java/com/spring/boot/server/demo/controller/HelloController.java
@@ -1,14 +1,23 @@
 package com.spring.boot.server.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    //localhost:8888/my_first_router
+    @Value("${current.env}")
+    private String env;
+
+    //localhost:port/my_first_router
     @RequestMapping("/my_first_router")
     public String index() {
         return "hello world!";
+    }
+
+    @RequestMapping("/current_env")
+    public String env(){
+        return env;
     }
 }
