6dfb146a3582d13eebfdeb580f4e37c82f280b2a
xin.guo <g71x88@gmail.com>
2017-10-16 16:42:30 +0800

    lombok constructor

--- src/main/java/com/spring/boot/server/demo/advice/ApiAdviceHandler.java
+++ src/main/java/com/spring/boot/server/demo/advice/ApiAdviceHandler.java
@@ -1,35 +1,12 @@
 package com.spring.boot.server.demo.advice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ApiAdviceHandler<T> {
     private int status;
     private String msg;
     private T data;
-
-    public ApiAdviceHandler(int status, String msg, T data) {
-        this.status = status;
-        this.msg = msg;
-        this.data = data;
-    }
-
-    //getter必写 return json
-    public int getStatus() {
-        return status;
-    }
-
-    public String getMsg() {
-        return msg;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    @Override
-    public String toString() {
-        return "ApiAdviceHandler{" +
-                "status=" + status +
-                ", msg='" + msg + '\'' +
-                ", data=" + data +
-                '}';
-    }
 }
